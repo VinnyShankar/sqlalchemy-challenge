18a0896e32f8aa58b7a9cdc770977903f331b70f
VinnyShankar <vinayakshank@gmail.com>
2023-05-05 19:08:26 -0700

    flask routes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -26,4 +26,17 @@ Station = Base.classes.station
 #################################################
 # Flask Setup
 #################################################
-app = Flask(__name__)  
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def welcome():
+    """List all available routes."""
+    return(f"Available Routes:<br/>"
+           f"/api/v1.0/precipitation<br/>"
+           f"/api/v1.0/stations<br/>"
+           f"/api/v1.0/tobs<br/>"
+           f"/api/v1.0/<start><br/>"
+           f"/api/v1.0/<start>/<end>")
